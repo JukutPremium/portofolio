0f82ee372300b983aafcbba06be0d32f86bdb0d0
JukutPremium <diandradiandra218@gmail.com>
2024-10-20 02:29:29 +0700

    first commit

--- javascript/navbar.js
+++ javascript/navbar.js
@@ -2,12 +2,10 @@ const menuBtn = document.getElementById('menu-btn');
     const mobileMenu = document.getElementById('mobile-menu');
     const mobileLinks = document.querySelectorAll('.mobile-link');
 
-    // Toggle mobile menu visibility with animation
     menuBtn.addEventListener('click', () => {
         mobileMenu.classList.toggle('open');
     });
 
-    // Close mobile menu when a link is clicked
     mobileLinks.forEach(link => {
         link.addEventListener('click', () => {
             mobileMenu.classList.remove('open');
