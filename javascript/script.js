e0da524a932f69995783a25d1b0fff2b13aa4c3a
JukutPremium <diandradiandra218@gmail.com>
2024-10-20 18:11:47 +0700

    add footer

--- javascript/script.js
+++ javascript/script.js
@@ -1,46 +1,52 @@
-    // Navbar (Mobile)
+// Navbar (Mobile)
+const menuBtn = document.getElementById('menu-btn');
+const mobileMenu = document.getElementById('mobile-menu');
+const mobileLinks = document.querySelectorAll('.mobile-link');
+
+menuBtn.addEventListener('click', () => {
+    mobileMenu.classList.toggle('open');
+});
+
+mobileLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        mobileMenu.classList.remove('open');
+    });
+});
+
+// to-top button
+const toTopButton = document.getElementById("to-top");
+window.addEventListener("scroll", () => {
+    if (window.scrollY > 200) {
+        toTopButton.classList.remove("hidden");
+    } else {
+        toTopButton.classList.add("hidden");
+    }
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    const navbar = document.getElementById('navbar');
+
+    // Animation
+    anime({
+        targets: navbar,
+        translateY: ['-100%', '0%'],
+        duration: 2500,
+        easing: 'easeOutExpo'
+    });
+
+    // Mobile Menu
     const menuBtn = document.getElementById('menu-btn');
     const mobileMenu = document.getElementById('mobile-menu');
-    const mobileLinks = document.querySelectorAll('.mobile-link');
 
-    menuBtn.addEventListener('click', () => {
-        mobileMenu.classList.toggle('open');
+    menuBtn.addEventListener('click', function () {
+        mobileMenu.classList.toggle('hidden');
     });
+});
 
-    mobileLinks.forEach(link => {
-        link.addEventListener('click', () => {
-            mobileMenu.classList.remove('open');
-        });
-    });
+document.addEventListener("DOMContentLoaded", function () {
+    document.body.classList.add("animate-pulse");
+});
 
-    // to-top button
-    const toTopButton = document.getElementById("to-top");
-    window.addEventListener("scroll", () => {
-        if (window.scrollY > 200) {
-            toTopButton.classList.remove("hidden");  
-        } else {
-            toTopButton.classList.add("hidden"); 
-        }
-    });
-
-    document.addEventListener('DOMContentLoaded', function() {
-        const navbar = document.getElementById('navbar');
-        
-        // Animation
-        anime({
-            targets: navbar,
-            translateY: ['-100%', '0%'],
-            duration: 2500,
-            easing: 'easeOutExpo'
-        });
-
-        // Mobile Menu
-        const menuBtn = document.getElementById('menu-btn');
-        const mobileMenu = document.getElementById('mobile-menu');
-        
-        menuBtn.addEventListener('click', function() {
-            mobileMenu.classList.toggle('hidden');
-        });
-    });
-
-
+window.addEventListener("load", function () {
+    document.body.classList.remove("animate-pulse");
+});
