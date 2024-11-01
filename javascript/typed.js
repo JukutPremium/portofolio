cdfcf713c27cf67501b273c6021878533fac6347
JukutPremium <diandradiandra218@gmail.com>
2024-11-01 10:43:01 +0700

    hehe

--- javascript/typed.js
+++ javascript/typed.js
@@ -1,6 +1,14 @@
 new Typed('#nama-saya', {
     strings: ['Shaka Pratama.'],
-    typeSpeed: 75,
+    typeSpeed: 100,
+    startDelay:2575,
+    showCursor: false,
+    loop: false,
+});
+
+new Typed('#nama-eug', {
+    strings: ['Shaka Pratama.'],
+    typeSpeed: 100,
     startDelay:2575,
     showCursor: false,
     loop: false,
